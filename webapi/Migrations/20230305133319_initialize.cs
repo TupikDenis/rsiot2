da7a5176424868a89b81b5a3f5d7db2140ac87f7
TupikDenis <tdenis.20012510@gmail.com>
2023-03-05 16:33:50 +0300

    fixed migrations

---
+++ webapi/Migrations/20230305133319_initialize.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentDate",
+                table: "Marks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Marks",
+                table: "Marks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Marks",
+                table: "Marks");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AppointmentDate",
+                table: "Marks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
