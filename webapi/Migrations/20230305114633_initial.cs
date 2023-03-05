d340d33cbeb3580c5695b4968e77bd24505db895
TupikDenis <tdenis.20012510@gmail.com>
2023-03-05 15:14:27 +0300

    fixed

---
+++ webapi/Migrations/20230305114633_initial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mark",
+                table: "Marks",
+                newName: "AppointmentDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AppointmentDate",
+                table: "Marks",
+                newName: "Mark");
+        }
+    }
+}
